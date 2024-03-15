2fbac7c2beefdac83561b0b4dc25e062729bb827
Santiago Toyos <ppikachu@gmail.com>
2024-03-15 15:24:46 -0300

    Update SiteFooter.vue

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer class="w-full flex flex-col justify-between items-center mt-8">
 
-		<div class="flex flex-col md:flex-row h-full sm:justify-center md:gap-8 md:items-center text-gray-400 text-sm">
+		<div class="flex flex-col md:flex-row h-full items-center md:gap-8 md:items-center text-gray-400 text-sm">
 			<p>©️ {{ new Date().getFullYear() }} Santiago Toyos</p>
 			<Social />
 		</div>

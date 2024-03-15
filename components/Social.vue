bf74b4a483c6c4ba891fa1dd8465bbee2b018316
Santiago Toyos <ppikachu@gmail.com>
2024-03-15 15:36:57 -0300

    Update Social.vue

--- components/Social.vue
+++ components/Social.vue
@@ -10,7 +10,7 @@ import data from '~/assets/social_links.json'
 			color="black"
 			:padded="false"
 		>
-			<UIcon :name="item.icon" dynamic class="w-5 h-5 sm:w-6 sm:h-6" />
+			<UIcon :name="item.icon" dynamic class="w-6 h-6" />
 		</UButton>
 	</section>
 </template>

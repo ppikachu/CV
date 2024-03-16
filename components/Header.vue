b29868811d7b912630da32d082bd1c5b15bd87fc
Santiago Toyos <ppikachu@gmail.com>
2024-03-16 16:41:32 -0300

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -13,7 +13,7 @@
 				/>
 				<Social />
 			</div>
-			<div id="switches" class="flex flex-row sm:items-start gap-2 sm:gap-4 mt-1">
+			<div id="switches" class="flex flex-row sm:items-start gap-2 sm:gap-4 mt-1 h-fit">
 				<!-- <UAvatar src="/images/avatar-128x128.png" alt="Santiago's avatar" :ui="{ size: { sm: 'w-24 h-24' } }" /> -->
 				<LocaleSwitch />
 				<LightDarkSwitch />

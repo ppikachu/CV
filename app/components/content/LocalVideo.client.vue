10d4e03f6dfae2beb2b867dc625c06744aa160b0
Santiago Toyos <ppikachu@gmail.com>
2024-08-26 14:41:42 -0300

    LocalVideo

---
+++ app/components/content/LocalVideo.client.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+//@ts-ignore
+import { VideoPlayer } from 'vue-md-player'
+import 'vue-md-player/dist/style.css'
+/* Define props */
+const props = defineProps({
+	src: {
+		type: String,
+	},
+	poster: {
+		type: String,
+		default: '/video/video_placeholder.png'
+	}
+})
+</script>
+
+<template>
+	<!--videos component-->
+	<section v-if="props.src" class="not-prose mt-8">
+		<VideoPlayer :src="props.src" :poster="props.poster" :width="640" :height="360" playsinline class="aspect-video rounded-t-lg" />
+		<UAlert variant="black" v-if="$slots.default" icon="i-mdi-movie" :ui="{ rounded: 'rounded-b-lg rounded-t-none' }">
+			<template #description>
+				<slot />
+			</template>
+		</UAlert>
+	</section>
+</template>
